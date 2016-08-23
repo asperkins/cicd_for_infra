d3ef6c2f036a3b0316e739c22735f0a30146591a
asperkins <tonyperkins@yahoo.com>
2016-08-22 21:20:42 -0500

    Initial Commit

---
+++ loadbalance/bin/destroy.sh
@@ -0,0 +1,6 @@
+terraform destroy\
+  -var "do_token=\"${DO_PAT}\""\
+  -var "pub_key=\"$WORKSPACE/.infra/keys/id_rsa.pub\""\
+  -var "pvt_key=\"$WORKSPACE/.infra/keys/id_rsa\""\
+  -var "ssh_fingerprint=\"$SSH_FINGERPRINT\""\
+  .infra/terraform
